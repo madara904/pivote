21543b083ce7da24e012f74e1d670d7f20bf1d6d
madara904 <gundogduberay@gmail.com>
2025-09-11 23:03:51 +0200

    org type etc

--- src/app/(dashboard)/dashboard/shipper/page.tsx
+++ src/app/(dashboard)/dashboard/shipper/page.tsx
@@ -2,13 +2,14 @@ import { requireShipperAccess } from "@/lib/auth-utils";
 import ShipperInquiryView from "./components/shipper-inquiry-view";
 
 const ShipperDashboard = async () => {
-  const { user, organization } = await requireShipperAccess();
+  const { orgType, user} = await requireShipperAccess();
+
 
   return (
     <div>
       <h1>Shipper Dashboard</h1>
-      <p>Welcome, {user.name} from {organization?.name}</p>
-      <p>Organization Type: {organization?.type}</p>
+      <p>Welcome, {user.name} from {orgType}</p>
+      <p>Organization Type: {orgType}</p>
 
       <ShipperInquiryView />
     </div>
